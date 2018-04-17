67385f6472e0dc47c0f697df8233c973a97c4b74
Evadina Sloate <ems16j@my.fsu.edu>
2018-04-17 15:06:28 +0000

    lesson 8 final commit

--- Movie.js
+++ Movie.js
@@ -32,7 +32,7 @@ $(document).ready(function(){
 
             $.ajax({
                 //Insert YOUR API Key after the '=' inside the single quotes.
-                url:'https://api.themoviedb.org/3/search/movie?api_key=INSERT-HERE',
+                url:'https://api.themoviedb.org/3/search/movie?api_key=40dd1aedc16c8674a067fc6221440634&query=big',
                 data: query
             })
             
@@ -57,10 +57,11 @@ $(document).ready(function(){
                  
                  *********************************Insert Code Below********************************************/                
                 
-                
+                 $("#overview").html(json.results[0].overview);
+
                 
 
-                /***********************Retrieve Movie Release Date from Server & Add to HTML Document***********
+                 /***********************Retrieve Movie Release Date from Server & Add to HTML Document***********
                  TASK #7:  
                  1.  Include the jQuery code below that will retrieve the movie release date from JSON object
                      and place on the HTML page.
@@ -72,6 +73,7 @@ $(document).ready(function(){
                  
                  *********************************Insert Code Below********************************************/               
 
+                $("#release_date").html(json.results[0].release_date);
 
 
 
@@ -87,7 +89,8 @@ $(document).ready(function(){
                  
                  *********************************Insert Code Below********************************************/   
 
-  
+                  $("#popularity").html(json.results[0].popularity);
+
 
                 
                  /*****************Retrieve Movie Original Language from Server & Add to HTML Document***********
@@ -101,8 +104,8 @@ $(document).ready(function(){
                  3.  Use the example above where the Movie Title was retrieved to complete this step!
                  
                  *********************************Insert Code Below********************************************/                 
-
-
+               
+               $("#original_language").html(json.results[0].original_language);
 
                 //The following code retrieves the movie poster path from the JSON object retrieved from the TMDB 
                 //Server and places the poster image on the HTML page, where the id="poster_path".  It took TWO
--- README.md
+++ README.md
@@ -4,19 +4,19 @@
 LIS 2360:  Web Application Development
 
 ### Assignment Date:  
-(Month Day, Year)
+(April 16, 2018)
 
 ### Student Name:  
-(First and Last Name)
+(Evadina Sloate)
 
 ### Project Description:
-(In your own words, briefly describe the assignment.)
+(For this project we learned how to build an application that displays information about movies.)
 
 ### View Project:
 (Replace this statement with your Github Page URL that was created when you 
  published the project.)
 
 ### Lessons Learned in the Assignment:
-1. (Briefly describe a lesson/concept learned in this lesson.)
-2. (Briefly describe a lesson/concept learned in this lesson.)
-3. (Briefly describe a lesson/concept learned in this lesson.)
+1. (JSON stands for JavaScript Object Notation.)
+2. (You retrieve the value by using the key/name as follows:  json.results[0].release_date)
+3. (Must open Ajax files from a web server with the HTTP (http://) or HTTPS (https://) protocol.)
